371fb9665777002217ef94ef7d6086313eb24d7a
Tom Faulhaber <git_net@infolace.com>
2013-03-10 23:12:38 -0700

    Add support for authorization and (effectively) disable paging, because we were over-running github limits

--- src/autodoc_index/indices.clj
+++ src/autodoc_index/indices.clj
@@ -1,8 +1,10 @@
 (ns autodoc-index.indices
   "Code for accessing github and building an index of all autodoc for an organization"
   (:require [clj-http.client :as client]
-            [clojure.data.json :as json])
-  (:use [clojure.pprint :only [cl-format]]))
+            [clojure.data.json :as json]
+            [clojure.string :as str])
+  (:use [clojure.pprint :only [cl-format]])
+  (:import [java.io File]))
 
 (defn user-url
   ([user] (user-url user false))
@@ -10,31 +12,32 @@
      (cl-format nil "https://api.github.com/~:[users~;orgs~]/~a"
                 org? user-or-org)))
 (defn repos
-  [base]
-  (let [url (str base "/repos") 
-        result (-> (client/get url {:accept :json}) :body
+  [base auth-str]
+  (let [url (str base "/repos?page=1&per_page=10000")
+        result (-> (client/get url {:accept :json :basic-auth auth-str}) :body
                    (#(java.io.StringReader. %)) json/read-json)]
     (for [r result] (:name r))))
 
-(defn branches [user repo]
-  (let [url (cl-format nil "https://api.github.com/repos/~a/~a/branches" user repo)]
-   (for [r (-> (client/get url {:accept :json}) :body
+(defn branches [user repo auth-str]
+  (let [url (cl-format nil "https://api.github.com/repos/~a/~a/branches?page=1&per_page=10000" user repo)]
+   (for [r (-> (client/get url {:accept :json :basic-auth auth-str}) :body
                (#(java.io.StringReader. %)) json/read-json)]
      (:name r))))
 
-(defn gh-pages-sha [user repo]
+(defn gh-pages-sha [user repo auth-str]
   (binding [*out* *err*] (println "gh-pages-sha" user repo))
-  (let [url (cl-format nil "https://api.github.com/repos/~a/~a/branches" user repo)
-        branches (-> (client/get url {:accept :json}) :body
+  (let [url (cl-format nil "https://api.github.com/repos/~a/~a/branches?page=1&per_page=10000" user repo)
+        branches (-> (client/get url {:accept :json :basic-auth auth-str})
+                     :body
                      (#(java.io.StringReader. %)) json/read-json)]
    (when-let [branch (first (filter #(= "gh-pages" (:name %)) branches)) ]
      (get-in branch [:commit :sha]))))
 
 
-(defn index-file [user repo]
-  (when-let [sha (gh-pages-sha user repo)]
-    (let [url (cl-format nil "https://api.github.com/repos/~a/~a/git/trees/~a" user repo sha)
-          result (-> (client/get url {:accept :json}) :body
+(defn index-file [user repo auth-str]
+  (when-let [sha (gh-pages-sha user repo auth-str)]
+    (let [url (cl-format nil "https://api.github.com/repos/~a/~a/git/trees/~a?page=1&per_page=10000" user repo sha)
+          result (-> (client/get url {:accept :json :basic-auth auth-str}) :body
                      (#(java.io.StringReader. %)) json/read-json)
           tree (:tree result)]
       (when-let [file-desc (last
@@ -43,12 +46,15 @@
                                                           (:path %))
                                              tree)))]
         (let [file-url (:url file-desc)]
-          (-> (client/get file-url {:accept "application/vnd.github.beta.raw"})
+          (-> (client/get file-url {:accept "application/vnd.github.beta.raw"
+                                    :basic-auth auth-str})
               :body read-string))))))
 
 (defn all-indices-by-repo [user org?]
-  (for [repo  (sort (repos (user-url user org?)))]
-    [repo (index-file user repo) ]))
+  (let [auth-str (str/trim-newline
+                  (slurp (File. (System/getenv "HOME") ".github-auth")))] ; needs to contain "username:password"
+    (for [repo  (sort (repos (user-url user org?) auth-str))]
+      [repo (index-file user repo auth-str)])))
 
 (defn one-namespace?
   "Takes a list of namespaces and returns true if there's only a single

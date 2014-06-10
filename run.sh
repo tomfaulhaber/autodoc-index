86913fae5acf890c30da9e8a154493d8d81a852b
Tom Faulhaber <git_net@infolace.com>
2014-06-09 22:57:06 -0700

    Don't try to do the commit if the Clojure code returns a non-zero exit code

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
-#!/bin/bash 
+#!/bin/bash
 
 lein run
+status=$?
+
+if [ ${status} -ne 0 ]
+then
+    echo "Index builder exited with status: ${status}, aborting..."
+    exit 1
+fi
+
 (cd ../autodoc-work-area/clojure.github.com; git add .; git commit -m"Automated index build"; git push origin master)

f75f13608db288bb2db8fbd9e48a48226de3c096
Tom Faulhaber <git_net@infolace.com>
2014-06-09 22:54:46 -0700

    Update dependencies to something a little more modern

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject autodoc-index "0.1.0-SNAPSHOT"
+(defproject autodoc-index "0.2.0-SNAPSHOT"
   :description "Build an overview of Clojure and all the documented contrib libraries based on the generated autodoc"
   :main autodoc-index.main
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/data.json "0.1.3"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.4"]
                  [enlive "1.0.0"]
-                 [clj-http "0.3.3"]]
+                 [clj-http "0.9.2"]]
   :dev-dependencies [])
